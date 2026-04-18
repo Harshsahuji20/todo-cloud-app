6658e8d9605d9d8d26af5ca68aa633475d985a86
HarshSahuji20 <hmsahuji20@gmail.com>
2026-04-19 01:17:38 +0530

    fix cors credentials issue

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI(title="Cloud Based To-Do List API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # allow all (important)
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
